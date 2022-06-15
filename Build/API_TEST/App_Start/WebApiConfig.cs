3942bb22ffe148fe9df4b426807e36afd69a94ca
Agushadad <agushadad@gmail.com>
2022-06-15 18:56:19 -0300

    Multiple GET methods
    Now it's possible to have multiple GET methods indicating an action name

--- Build/API_TEST/App_Start/WebApiConfig.cs
+++ Build/API_TEST/App_Start/WebApiConfig.cs
@@ -16,8 +16,8 @@ namespace API_TEST
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { action = RouteParameter.Optional, id = RouteParameter.Optional }
             );
         }
     }
--- Build/API_TEST/Controllers/ValuesController.cs
+++ Build/API_TEST/Controllers/ValuesController.cs
@@ -1,36 +1,65 @@
 ﻿using API_TEST.Core;
 using System.Collections.Generic;
 using System.Web.Http;
-using API_TEST.Core;
 using System.Linq;
 using API_TEST.Models;
+using System.Text;
+using System;
 
 namespace API_TEST.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<Personajes> Get()
+        [HttpGet]
+        public StringBuilder Get()
         {
             using (Manager objConnection = new Manager())
             {
-                objConnection.Connect();
-                return objConnection.Factory.Personajes.AsEnumerable();
-            }
+                StringBuilder resultado = new StringBuilder();
+                var lstpersonajes = objConnection.Factory.Personajes.ToList();
+                var lista = new List<string>();
+                foreach (var personaje in lstpersonajes)
+                    resultado.Append($"[{personaje.Id} - {personaje.Nombre} {personaje.Apellido} - {personaje.Serie}]");
+                return resultado;
+            }            
         }
 
         // GET api/values/5
         [HttpGet]
+        [ActionName("GetName")]
         public string GetNameById(int id)
         {
             using (Manager objConnection = new Manager())
             {
                 objConnection.Connect();
-                var name = objConnection.Factory.Personajes.Where(x => x.Id == id).Select(x => x.Nombre).FirstOrDefault();            
+                var name = objConnection.Factory.Personajes.Where(x => x.Id == id).Select(x => x.Nombre).FirstOrDefault();
                 return name;
             }
         }
-
+        [HttpGet]
+        [ActionName("GetLastName")]
+        public string GetLastNameById(int id)
+        {
+            using (Manager objConnection = new Manager())
+            {
+                objConnection.Connect();
+                var lastName = objConnection.Factory.Personajes.Where(x => x.Id == id).Select(x => x.Apellido).FirstOrDefault();
+                return lastName;
+            }
+        }
+        [HttpGet]
+        [ActionName("GetFullName")]
+        public string GetFullNameById(int id)
+        {
+            using (Manager objConnection = new Manager())
+            {
+                objConnection.Connect();
+                var objPersonaje = objConnection.Factory.Personajes.Where(x => x.Id == id).FirstOrDefault();
+                var fullName = objPersonaje.Nombre + " " + objPersonaje.Apellido;
+                return fullName;
+            }
+        }      
         // POST api/values
         public void Post([FromBody] string value)
         {
@@ -41,9 +70,17 @@ namespace API_TEST.Controllers
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/values/5      
         public void Delete(int id)
         {
+            using (Manager objConexion = new Manager()) 
+            {
+                objConexion.Connect();
+                var objPersonaje = objConexion.Factory.Personajes.Where(x => x.Id == id).FirstOrDefault();
+                if (objPersonaje != null)
+                    objConexion.Factory.Personajes.DeleteOnSubmit(objPersonaje);
+                objConexion.Factory.SubmitChanges();
+            }
         }
     }
 }
